c6830c02bb6bc45e9c783990dfde93e9e533dfa8
Alvif Sandana Mahardika <asm240800@gmail.com>
2021-07-10 00:22:38 +0700

    add new layouts: profile

---
+++ README.md
@@ -0,0 +1,23 @@
+## Ekspedisi android
+
+Fitur yang sedang dikerjakan:
+
+#### Frontend
+- [x] Bottom Navigation
+- [ ] Fragment Home xml styles
+- [ ] Fragment Jadwal muatan xml styles
+- [ ] Fragment Notifikasi xml styles
+- [x] Fragment User Profile xml styles
+- [x] Fragment Register new user xml styles
+- [x] Fragment Login user xml styles
+- [ ] Fragment Tracking xml styles
+- [ ] Fragment List Tarif xml styles
+- [ ] Fragment Tentang kami xml styles
+
+#### Backend
+- [ ] Register new user
+- [ ] Login new user
+- [ ] show notifikasi
+- [ ] show list tarif
+- [ ] user profile (update profile info and password)
+- [ ] retrieve data from restAPI
--- app/src/main/java/com/app/ekspedisimlg/MainActivity.kt
+++ app/src/main/java/com/app/ekspedisimlg/MainActivity.kt
@@ -2,12 +2,43 @@ package com.app.ekspedisimlg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // force disable night mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        // define menu
+        var menu: Menu = bottom_navigation.menu
+        // get menu item
+        selectedMenu(menu.getItem(0))
+        // set listener for selected menu item
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            item: MenuItem -> selectedMenu(item)
+            false
+        }
+    }
+
+    // open and change fragment when user selected menu item
+    fun selectedFragment(fragment: Fragment){
+        var transaction: FragmentTransaction? = supportFragmentManager.beginTransaction()
+        transaction?.replace(R.id.rootFragment, fragment)
+        transaction?.commit()
+    }
+
+    // menu handler
+    private fun selectedMenu(item: MenuItem){
+        item.isChecked = true
+        when(item.itemId){
+
+        }
     }
 }
