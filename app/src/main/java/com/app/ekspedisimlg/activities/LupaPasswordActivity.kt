0e74f206fed01e24346ce9ac86b06893b3c986e8
AlvifSandana <asm240800@gmail.com>
2021-10-10 10:12:43 +0700

    create forget password, re-layout

---
+++ app/src/main/java/com/app/ekspedisimlg/activities/LupaPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.app.ekspedisimlg.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import com.app.ekspedisimlg.R
+import android.os.Bundle
+
+class LupaPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lupa_password)
+    }
+}
